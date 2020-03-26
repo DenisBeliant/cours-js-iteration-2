8fc814165fbd84873eb2774cbc16ccab345db2ef
DenisBeliant <58740617+DenisBeliant@users.noreply.github.com>
2020-03-26 15:34:28 +0100

    All it's done vanilla !

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -6,12 +6,62 @@
  *    - charger les données des objets dans la table
  */
 
+var object_data = {
+    "serial": "OBJ_001",
+    "type": "raspberry_TH",
+    "image": "raspberry-pi-4.jpg",
+    "description": "Capteur de température et d'humidité de la salle de cours du Campus de Chambéry",
+    "location": "45.644065, 5.867810",
+    "refresh": 5,
+    "status": true,
+    "provisionning":{
+        "date": "2020-03-20",
+        "operator": "JPA"
+    }
+};
+
 function load_components() {
     console.log("Chargement des données de la page");
     // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
-    $.get('/objects', function(d) { console.log(d); });
+    $.get('/objects', function(data) { 
+
+        for(let e of data.objects) {
+
+            add_line_to_table(e);
+        }
+     });
 }
 
+function add_line_to_table(data) {
+
+    let line = '<tr>\
+    <td>'+data.serial+'</td>\
+    <td><img src="/static/images/'+data.image+'" width="40px" height="40px"/></td>\
+    <td>'+data.description+'</td>\
+    <td><input type="checkbox" /></td>\
+    <td><input type="button" value="Détails" class="btn-outline-warning"/></td>\
+</tr>';
+document.getElementById('table_body').innerHTML += line;
+
+// $('#table_body').append(line);
+}
 // Solution 1 :
-load_components();
+// load_components();
+
+function test_add_line() {
+    return data = {
+        "serial": "OBJ_001",
+        "type": "raspberry_TH",
+        "image": "raspberry-pi-4.jpg",
+        "description": "Capteur de température et d'humidité de la salle de cours du Campus de Chambéry",
+        "location": "45.644065, 5.867810",
+        "refresh": 5,
+        "status": true,
+        "provisionning":{
+            "date": "2020-03-20",
+            "operator": "JPA"
+        }
+    };
+}
+
 
